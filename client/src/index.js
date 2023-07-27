a926f8f6111c0dc9240c47cd8854a2e70ea9e9a0
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-07-26 21:26:07 -0500

    added theme file and imported it into index.js to use in the project. In theme file the color pallette for project is laid out.

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ client/src/theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#42a5f5',
+        main: '#1976d2',
+        dark: '#1565c0',
+      },
+      success: {
+        light: '#4caf50',
+        main: '#2e7d32',
+        dark: '#1b5e20',
+      }
+    }
+})
+
+export default theme;
