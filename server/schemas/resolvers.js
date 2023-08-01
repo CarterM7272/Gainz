b80f8d6aafded8667b4f051c5c191058880b3525
jtpheonix13 <jgtihunt@comcast.net>
2023-07-31 19:39:11 -0500

    worked with caleb to get api calls on the server side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,25 @@
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError, UserInputError } = require('apollo-server-express');
-const { dateScalar } = require('./scalar');
+const { dateScalar, jsonScalar } = require('./scalar');
+
+let fetch;
+(async () => {
+  const fetchModule = await import('node-fetch');
+  fetch = fetchModule.default;
+})();
+
+const exerciseOptions = {
+  method: 'GET',
+  headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
 
 const resolvers = {
   Date: dateScalar,
+  JSON: jsonScalar,
   Query: {
     me: async (parent, args, context) => {
       if (!context.user) {
@@ -12,6 +27,22 @@ const resolvers = {
       }
       return await User.findById(context.user._id)
     },
+    getFromExerciseDb: async (parent, { queryString }, context) => {
+      try {
+
+        const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${queryString ? queryString : ''}`
+        const options = exerciseOptions;
+
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        return data;
+    
+      } catch (err) {
+        console.log(err);
+      };
+    },
+    // getWorkouts: async ()
   },
   Mutation: {
     addUser: async (parent, argObj) => {
