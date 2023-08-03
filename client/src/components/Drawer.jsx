3b97829f15973c76c5e2718690b8e24038f800dc
jtpheonix13 <jgtihunt@comcast.net>
2023-08-02 19:41:29 -0500

    fixed duplicat key error

--- client/src/components/Drawer.jsx
+++ client/src/components/Drawer.jsx
@@ -41,7 +41,7 @@ export default function PermanentDrawerLeft() {
     <Divider />
     <List>
       {['', '', ''].map((text, index) => (
-        <ListItem key={text} disablePadding>
+        <ListItem key={index} disablePadding>
           <ListItemButton>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
