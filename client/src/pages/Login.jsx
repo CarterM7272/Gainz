3f13548c2b52dc64ab69d89534c76d0f5b79e4a5
fcollazoabadia <f.collazoabadia@gmail.com>
2023-08-03 20:44:35 -0500

    fixed login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,6 +13,13 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../graphql/mutations";
+import { useSelector } from "react-redux";
+import { getUser } from "../redux/slices/userSlice";
+import { Navigate } from "react-router-dom";
+
 import Page from "../components/Page";
 import AuthService from "../utils/auth";
 import UnstyledLink from '../components/UnstyledLink';
@@ -30,17 +37,59 @@ function Copyright(props) {
   );
 }
 
-const defaultTheme = createTheme();
+const headContent = (
+  <>
+    <title>Login</title>
+    <meta name="description" content="Login for Project-3 Starter Code." />
+  </>
+);
 
-export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+
+export default function Login() {
+  const [loginUser, { error, data, loading }] = useMutation(LOGIN_USER);
+  const { isAuthenticated } = useSelector(getUser());
+
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
     });
   };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await loginUser({
+        variables: { ...formState },
+      });
+
+      AuthService.login(data.loginUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/dashboard"} />;
+  }
+
+// export default function SignIn() {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get('email'),
+//       password: data.get('password'),
+//     });
+//   };
 
   return (
   <Page isProtected={false} headContent={headContent}>
@@ -60,54 +109,58 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <UnstyledLink>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              color={'secondary'}
-            >
-              Sign In
-            </Button>
-            </UnstyledLink>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <form onSubmit={handleFormSubmit}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={handleChange}
+                value={formState.email}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleChange}
+                value={formState.password}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                color={'secondary'}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
+            </form>
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
@@ -116,10 +169,3 @@ export default function SignIn() {
 
   );
 }
-
-const headContent = (
-  <>
-    <title>Login</title>
-    <meta name="description" content="Login for Project-3 Starter Code." />
-  </>
-);
